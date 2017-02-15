f920935d6a8ef9a193a81ea30295a7b7c994416e
John.Lockwood <john.lockwood@workiva.com>
2017-02-15 15:36:39 -0800

    Use unicode for string arg

--- test/integration/python/common/test_definitions.py
+++ test/integration/python/common/test_definitions.py
@@ -16,7 +16,7 @@ def rpc_test_definitions():
 
     tests['testVoid'] = dict(args=None, expected_result=None)
 
-    thing = "thing"
+    thing = u"thing"
     tests['testString'] = dict(args=[thing], expected_result=thing)
 
     tests['testBool'] = dict(args=[True], expected_result=True)
